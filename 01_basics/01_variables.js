4c76fb4a545cd9b9cb0be60035de1392f8db4049
Puneet Singh <19bcs2594cu@gmail.com>
2024-09-10 02:36:20 +0000

    learning datatypes

--- 01_basics/01_variables.js
+++ 01_basics/01_variables.js
@@ -1,7 +1,19 @@
-const accountId = 24433;
+const accountId = 24433
 let accountEmail = "aaa@gmail.com"
 var accountPassword = "12345"
 accountCity = "Jaipur"
+let accountState;
 
-console.log();
+//accountId = 23 //not allowed
+console.log(accountId);
 
+accountEmail = "bbb@gmail.com"
+accountPassword = "9887"
+accountCity = "IXR"
+
+/*
+ALWAYS use LET
+because of issue in block scope and functional scope
+*/
+
+console.table([accountEmail, accountPassword, accountCity, accountId, accountState])
---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,22 @@
+"use strict"; // treat all JS code as newer version
+
+// alert () // we are using nodeJs and not a browser
+
+let name = "thinQ"
+let age = 28
+let isLoggedIn = true
+
+// number
+// bigint
+// string => "" , ''
+// boolean => true , false
+// null => standalone value
+// undefined
+// symbol => unique
+
+// object
+
+console.log(typeof "jjj")
+
+console.log(typeof null) // object type
+console.log(typeof undefined) // undefined
