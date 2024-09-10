7704a72e30b3979a96756db9d7b19b5ed01d4788
Puneet Singh <19bcs2594cu@gmail.com>
2024-09-10 03:41:34 +0000

    conversion of data types

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,32 @@
+let score = "33"
+
+console.log(typeof score);
+console.log(typeof(score));
+
+let value = Number(score)
+console.log(value);
+console.log(typeof value);
+
+// "33" => 33
+// "33abs" => NaN
+// true => 1 , false => 0
+
+let isLoggedIn = "a";
+
+let isBooleanLoggedIn = Boolean(isLoggedIn)
+console.log(isBooleanLoggedIn);
+
+// 1 => true , 0 => false
+// "" => false
+// "abc" => true
+
+console.log("***");
+
+let someNumber = 33
+let stringNumber = String(someNumber)
+console.log(stringNumber)
+console.log(typeof stringNumber)
+
+
+
+
